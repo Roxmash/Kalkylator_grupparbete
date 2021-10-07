0092090bef5e734890ec27407cff4875ed538127
oscarsparrman <oscar.sparrman@student.kyh.se>
2021-10-07 15:11:07 +0200

    La in kommentarer.button tint. löst problem

--- app/src/main/java/com/brandent/kalkylator_grupparbete/MainActivity.java
+++ app/src/main/java/com/brandent/kalkylator_grupparbete/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,20 +15,25 @@ import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    double resultAsDouble;
-    double input1AsDouble;
-    double input2AsDouble;
+
     EditText input1;
     EditText input2;
     TextView resultText;
 
     boolean isTwoFieldShowed = true;
-    String inputStr1 = "";
-    String inputStr2 = "";
-    String tempStr = "";
     String currentOperation = "+";
-
     ImageView mathMode;
+    ImageButton buttonclear;
+    ImageButton buttonPlus;
+    ImageButton buttonMinus;
+    ImageButton buttonDivide;
+    ImageButton buttonRoot;
+    ImageButton buttonPercent;
+    ImageButton buttonMultiply;
+    ImageButton buttonPythagorean;
+    ImageButton buttonCylinderVolume;
+    ImageButton buttonCircleArea;
+    ImageButton buttonResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +44,37 @@ public class MainActivity extends AppCompatActivity {
         input2 = findViewById(R.id.input2);
         resultText =findViewById(R.id.display_result_tw);
         mathMode = findViewById(R.id.math_mode);
+        buttonclear = findViewById(R.id.button_erase);
+        buttonPlus = findViewById(R.id.button_plus);
+        buttonMinus = findViewById(R.id.button_minus);
+        buttonDivide = findViewById(R.id.button_divide);
+        buttonRoot = findViewById(R.id.button_square_root);
+        buttonPercent = findViewById(R.id.button_procent);
+        buttonMultiply = findViewById(R.id.button_multiply);
+        buttonPythagorean = findViewById(R.id.button_pythager);
+        buttonCylinderVolume = findViewById(R.id.button_cylinder_volume);
+        buttonCircleArea = findViewById(R.id.button_circle_area);
+        buttonResult =findViewById(R.id.button_result);
+
     }
+
+
+    public void tintButton(ImageButton button){
+
+         buttonPlus.setBackgroundTintList(getResources().getColorStateList(R.color.buttoncolour));
+         buttonclear.setBackgroundTintList(getResources().getColorStateList(R.color.buttoncolour));
+         buttonMinus.setBackgroundTintList(getResources().getColorStateList(R.color.buttoncolour));
+         buttonDivide.setBackgroundTintList(getResources().getColorStateList(R.color.buttoncolour));
+         buttonRoot.setBackgroundTintList(getResources().getColorStateList(R.color.buttoncolour));
+         buttonPercent.setBackgroundTintList(getResources().getColorStateList(R.color.buttoncolour));
+         buttonMultiply.setBackgroundTintList(getResources().getColorStateList(R.color.buttoncolour));
+         buttonPythagorean.setBackgroundTintList(getResources().getColorStateList(R.color.buttoncolour));
+         buttonCylinderVolume.setBackgroundTintList(getResources().getColorStateList(R.color.buttoncolour));
+         buttonCircleArea.setBackgroundTintList(getResources().getColorStateList(R.color.buttoncolour));
+         buttonResult.setBackgroundTintList(getResources().getColorStateList(R.color.buttoncolour));
+         button.setBackgroundTintList(getResources().getColorStateList(R.color.pressed_button));
+    }
+
 
     private void changeFieldsVisibility(boolean showTwoField){
         if(showTwoField){
@@ -46,12 +83,15 @@ public class MainActivity extends AppCompatActivity {
 
         }
             else {
-            input2.setText(input1.getText().toString());
+
+
             input1.setVisibility(View.GONE);
             isTwoFieldShowed = false;
         }
 
     }
+
+    // Ändrar till rätt bild brevid inmatningsfälten för respektive matteoperation.
 
     private void setImage(String imageName) {
 
@@ -62,19 +102,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //hämtar input från Edittext och gör om till Double
-    public String checkInput(EditText input){
-        String inputStr = input.getText().toString();
-
-        if(isNumeric(inputStr)) {
-            return inputStr;
-        } else {
-            return "ERROR";
-        }
-    }
 
 
-    // Gör om svar till en sträng och visar det i resultText.
+
+    // Gör om svar till en sträng med två decimaler och visar det i resultText.
     public void setResult(double result){
         DecimalFormat df = new DecimalFormat("#.00");
 
@@ -86,11 +117,13 @@ public class MainActivity extends AppCompatActivity {
 
     //Clear input field
     public void clearBtnClicked(View view) {
+        tintButton(buttonclear);
         input1.setText("");
         input2.setText("");
     }
 
     public void plusBtnClicked(View view) {
+        tintButton(buttonPlus);
         currentOperation = "+";
         input1.setHint("value 1");
         input2.setHint("value 2");
@@ -99,6 +132,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void minusBtnClicked(View view) {
+        tintButton(buttonMinus);
         currentOperation = "-";
         input1.setHint("value 1");
         input2.setHint("value 2");
@@ -107,6 +141,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void divideBtnClicked(View view) {
+        tintButton(buttonDivide);
         currentOperation = "/";
         input1.setHint("value 1");
         input2.setHint("value 2");
@@ -115,6 +150,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void rootBtnClicked(View view) {
+        tintButton(buttonRoot);
         currentOperation = "√";
         changeFieldsVisibility(false);
         input1.setHint("");
@@ -123,6 +159,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void percentBtnClicked(View view) {
+        tintButton(buttonPercent);
         currentOperation = "%";
         changeFieldsVisibility(true);
         input1.setHint("%");
@@ -131,6 +168,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void multiplyBtnClicked(View view) {
+        tintButton(buttonMultiply);
         currentOperation = "*";
         input1.setHint("value 1");
         input2.setHint("value 2");
@@ -138,7 +176,8 @@ public class MainActivity extends AppCompatActivity {
         setImage("multiply");
     }
 
-    public void pyfagorBtnClicked(View view) {
+    public void pythagorBtnClicked(View view) {
+        tintButton(buttonPythagorean);
         currentOperation = "pythagoras";
         input1.setHint("value a");
         input2.setHint("value b");
@@ -147,8 +186,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-            //Ska vara cylinder volym
+
     public void cylinderBtnClicked(View view) {
+        tintButton(buttonCylinderVolume);
         currentOperation = "cylinder";
         changeFieldsVisibility(true);
         input1.setHint("Radie");
@@ -157,13 +197,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void circleBtnClicked(View view) {
+        tintButton(buttonCircleArea);
         currentOperation = "circle";
         input2.setHint("Radie");
         changeFieldsVisibility(false);
         setImage("sphere");
     }
 
+  /* Kollar om det är ett siffervärde i inmatningsfälten.
+     Om det är ett siffervärde använder den det annars Error.
+     Om det inte är Error och fälten är ifyllda med  så tar vi in inputsen och gör om
+     till doubles för sen skicka in i mattefunktionerna.
+*/
+
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
+
+    public String checkInput(EditText input){
+        String inputStr = input.getText().toString();
+
+        if(isNumeric(inputStr)) {
+            return inputStr;
+        } else {
+            return "ERROR";
+        }
+    }
+
     public void resultBtnClicked(View view) {
+         tintButton(buttonResult);
         if( checkInput(input1) != "ERROR" && checkInput(input2) != "ERROR" && isTwoFieldShowed) {
             double input1AsDouble = Double.parseDouble(input1.getText().toString());
             double input2AsDouble = Double.parseDouble(input2.getText().toString());
@@ -175,6 +242,9 @@ public class MainActivity extends AppCompatActivity {
             resultText.setText("ERROR");
         }
     }
+
+
+
 
     private void switchOperation(double in1, double in2) {
         CalcMath calc = new CalcMath(in1, in2);
@@ -223,14 +293,8 @@ public class MainActivity extends AppCompatActivity {
                 toast.show();
         }
         setResult(result);
+
     }
 
-    public static boolean isNumeric(String str) {
-        try {
-            Double.parseDouble(str);
-            return true;
-        } catch(NumberFormatException e){
-            return false;
-        }
-    }
+
 }
